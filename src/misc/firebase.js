9f836cc46c15a981723139d93f778cdd55c92425
NitikaRawat <nitikar18@gmail.com>
2021-12-03 00:06:54 +0530

    Created Sign in page with google sign in

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: "AIzaSyCfsIltklKQbJoUZbsZqrzK6DRRJH4HFOg",
@@ -12,3 +14,5 @@ const config = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Panel, Col, Button, Icon, Alert} from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
-    return <div>SignIn</div>;
+    
+    const signInWithProvider = async (provider) =>{
+        try
+        {
+        const { additionalUserInfo, user} = await auth.signInWithPopup(provider); 
+        
+        if(additionalUserInfo.isNewUser){
+             await database.ref(`/profiles/${user.uid}`).set({
+              name : user.displayName,
+              createdAt : firebase.database.ServerValue.TIMESTAMP
+          });
+        }
+         Alert.success('Signed-In', 4000);
+        }
+
+        catch(err)
+        {
+            Alert.error(err.message, 4000);
+
+        }
+
+    };
+
+   const onGoogleSignIn = () => {
+        signInWithProvider(new firebase.auth.GoogleAuthProvider());
+    };
+
+
+    return( 
+        <Container>
+        <Grid className="mt-page">
+        <Row>
+        <Col xs={24} md={12} mdOffset={6}>
+        <Panel>
+        <div className = "text-center">
+        <h1>
+            Welcome To SNEEK 
+        </h1>
+        <p>
+            Progressive chat platform for everyone
+        </p>
+        </div>
+        <div className = "mt-3">
+            <Button block color="green" onClick={onGoogleSignIn}>
+                <Icon icon = "google" /> Google Sign-In
+            </Button>
+        </div>
+        </Panel>
+        </Col>
+        </Row>
+        </Grid>
+        </Container>
+    );
 
 };
 
